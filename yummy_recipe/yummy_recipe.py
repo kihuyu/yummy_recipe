ab822db9e8b7958d213fedb89988c4a08b88d9b0
Maina <maina@localhost.localdomain>
2017-10-18 23:53:28 +0300

    UPDATE TO PROGRAM PYTHON FILE

--- yummy_recipe/yummy_recipe.py
+++ yummy_recipe/yummy_recipe.py
@@ -1,10 +1,15 @@
 import os
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+from sqlalchemy.orm import sessionmaker
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from tabledef import User, Recipecategory, Recipe
+users = {}
+recipecategories = {}
+recipes = {}
+user_session = False
 
-app = Flask(__name__) 
-app.config.from_object(__name__) 
+app = Flask(__name__)
+app.config.from_object(__name__)
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'yummy_recipe.db'),
     SECRET_KEY='development key',
@@ -13,74 +18,69 @@ app.config.update(dict(
 ))
 app.config.from_envvar('YUMMY_RECIPE_SETTINGS', silent=True)
 
-def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
-
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
 
 
-def get_db():
-    """Opens a new database connection if there is none yet for the
-    current application context.
-    """
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
+
+
+@app.route('/recipecategory')
+def recipecategorys():
+    if user_session == False:
+        abort(401)
+    entries = recipecategories
+    recipez = recipes
+    print(str(recipecategories))
+    return render_template('recipecategory.html', entries=entries, recipez=recipez)
+
+
+@app.route('/addrecipecategory', methods=['GET','POST'])
+def add_recipe_category():
+    if user_session == False:
+        abort(401)
+    if request.method == 'POST':
+        newrecipe = str(request.form['recipecategory'])
+        recipecategories[newrecipe] = str(request.form['furtherinfo'])
+        flash('New category succesfully added')
+        return redirect('/recipecategory')
+    return render_template('addrecipecategory.html')
+
+@app.route('/addrecipe', methods=['GET','POST'])
+def add_recipe():
+    if user_session == False:
+        abort(401)
+    if request.method == 'POST':
+        newrecipe = str(request.form['recipename'])
+        recipes[newrecipe] = str(request.form['steps'])
+        flash('New Recipe succesfully added')
+        return redirect('/recipecategory')
+    return render_template('addrecipe.html')
+
 
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
         abort(401)
-    db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['title'], request.form['text']])
-    db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
-@app.route('/registration', methods=['POST'])
-def registration():
-    error = None
-    db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-                 [request.form['username'], request.form['password']])
-    db.commit()
-    flash('New user successfully added')
-    return redirect(url_for('login'))
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
+@app.route('/register', methods=['GET', 'POST'])
+def register():
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
+        users[request.form['username']] = request.form['password']
+        flash('New user successfully added')
+        return redirect('/')
+    return render_template('register.html')
+
+@app.route('/', methods=['GET','POST'])
+def login():
+ 
+    if request.method == 'POST':
+        key = request.form['username']
+        value = request.form['password']
+        if key in users and value == users[key]:
+            global user_session
+            user_session = True
+            return redirect('/recipecategory')
         else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
-
+            flash('Wrong credentials')
+    
+    return render_template('login.html')
