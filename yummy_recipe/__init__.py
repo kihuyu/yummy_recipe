73a8e73c7b959a7edc2172a40a230d381b5a703a
Maina <maina@localhost.localdomain>
2017-10-19 16:05:44 +0300

    heroku deploy

--- yummy_recipe/__init__.py
+++ yummy_recipe/__init__.py
@@ -1,1 +1,10 @@
 from .yummy_recipe import app
+from wtforms import Form, BooleanField, TextField, PasswordField, validators
+
+class Registration(Form):
+    username = TextField('Username')
+    name = TextField('Email Address')
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
--- yummy_recipe/schema.sql
+++ yummy_recipe/schema.sql
@@ -1,12 +1,20 @@
+drop table if exists users;
+create table users (
+  id         integer primary key autoincrement,
+  username   text not null,
+  password   text not null,
+  name       text not null
+);
 drop table if exists recipe_categorys;
 create table recipe_categorys (
-  id integer primary key autoincrement,
+  recipe_category_id   integer primary key autoincrement,
   recipe_category text not null,
-  further_info text not null
+  further_info    text not null
 );
 drop table if exists recipes;
 create table recipes (
-  id integer primary key autoincrement,
-  recipe text not null,
-  steps text not null
+  recipe_id                 integer primary key autoincrement,
+  recipe text               not null,
+  recipe_recipe_catedory_id integer,
+  FOREIGN KEY(recipe_recipe_catedory_id) REFERENCES recipe_categorys(recipe_category_id)
 );
